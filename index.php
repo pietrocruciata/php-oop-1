613fb4e59c883c86335a92ecb19c0679b09a329f
pietrocruciata <cruciata9748@gmail.com>
2024-05-10 14:07:36 +0200

    add: movies and tv series class extends production

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
-
-$films = [];
-
-
 class Production
 {
     public $title;
@@ -19,19 +15,49 @@ class Production
     }
 }
 
-$iron_man = new Production('Iron Man 2', 'English', 8, 'https://image.tmdb.org/t/p/w342//6WBeq4fCfn7AN0o21W9qNcRF2l9.jpg');
-$films[] = $iron_man;
+class Movie extends Production
+{
+    public $profit;
+    public $duration;
 
-$rocky = new Production('Rocky', 'English', 9, 'https://image.tmdb.org/t/p/w342//cqxg1CihGR5ge0i1wYXr4Rdeppu.jpg');
-$films[] = $rocky;
+    function __construct($_title, $_language, $_vote, $_image, $_profit, $_duration)
+    {
+        parent::__construct($_title, $_language, $_vote, $_image);
+        $this->profit = $_profit;
+        $this->duration = $_duration;
+    }
+}
 
-$barbie = new Production('Barbie', 'English', 9, 'https://image.tmdb.org/t/p/w342//iuFNMS8U5cb6xfzi51Dbkovj7vM.jpg');
-$films[] = $barbie;
+class TVSeries extends Production
+{
+    public $seasons;
 
-$il_padrino = new Production('Il Padrino', 'Italiano', 7, 'https://image.tmdb.org/t/p/w342//3bhkrj58Vtu7enYsRolD1fZdja1.jpg');
-$films[] = $il_padrino;
+    function __construct($_title, $_language, $_vote, $_image, $_seasons)
+    {
+        parent::__construct($_title, $_language, $_vote, $_image);
+        $this->seasons = $_seasons;
+    }
+}
 
 
+$iron_man = new Movie('Iron Man 2', 'English', 8, 'https://image.tmdb.org/t/p/w342//6WBeq4fCfn7AN0o21W9qNcRF2l9.jpg', '$ high', '2h');
+
+$rocky = new Movie('Rocky', 'English', 9, 'https://image.tmdb.org/t/p/w342//cqxg1CihGR5ge0i1wYXr4Rdeppu.jpg', '$ high', '2h');
+
+$barbie = new Movie('Barbie', 'English', 9, 'https://image.tmdb.org/t/p/w342//iuFNMS8U5cb6xfzi51Dbkovj7vM.jpg', '$ high', '2h 10m');
+
+$il_padrino = new Movie('Il Padrino', 'Italiano', 7, 'https://image.tmdb.org/t/p/w342//3bhkrj58Vtu7enYsRolD1fZdja1.jpg', '$ medium', '1h 30m');
+
+$films = [$il_padrino, $barbie, $rocky, $iron_man];
+
+$papel = new TVSeries('Casa the papel', 8, 'Espagnol', 'https://image.tmdb.org/t/p/w342//reEMJA1uzscCbkpeRJeTT2bjqUp.jpg', 5);
+$breaking_bad = new TVSeries('Breaking bad ', 8, 'English', 'https://image.tmdb.org/t/p/w342//ApitDfnYDwFNOPfyVuhW5ElWzhe.jpg', 5);
+$prison_break= new TVSeries('Prison break ', 10, 'English', 'https://image.tmdb.org/t/p/w342//h1QRyGuWVXM4Cr3FyPISSjMCBkA.jpg', 6);
+$vis= new TVSeries('Vis a Vis ', 9, 'Espagnol', '	https://image.tmdb.org/t/p/w342//1kH9u5DkoDUuIlhFFWjz9VSlKhC.jpg', 5);
+
+
+
+$series = [$papel, $breaking_bad, $prison_break, $vis];
 ?>
 
 <!DOCTYPE html>
@@ -49,22 +75,42 @@ $films[] = $il_padrino;
         <main>
             <div class="container">
                 <div class="row ">
-                    <div class=" d-flex justify-content-between flex-wrap">
+                    <h2 class="text-center p-3">MOVIES</h2>
+                    <div class=" d-flex flex-wrap ">
                         <?php
                         foreach ($films as $film) {
                         ?>
-                            <div class="col-6 text-center">
+                            <div class="col-3 text-center">
                                 <img src="<?php echo $film->image ?>" alt="">
-                                <h2><?php echo $film->title ?></h2>
-                                <div><?php echo $film->language ?></div>
-                                <div><?php echo $film->vote ?></div>
+                                <h2>TITOLO: <?php echo $film->title ?></h2>
+                                <div>LINGUA: <?php echo $film->language ?></div>
+                                <div>VOTAZIONE: <?php echo $film->vote ?></div>
+                                <div>PROFITTI: <?php echo $film->profit ?></div>
+                                <div>DURATA: <?php echo $film->duration ?></div>
+
+                            </div>
+                        <?php
+                        }
+                        ?>
+                        </div>
+                        <h2 class="text-center p-3">SEIRE TV</h2>
+                        <div class=" d-flex  flex-wrap ">
+                        <?php
+                        foreach ($series as $serie) {
+                        ?>
+                            <div class="col-3 text-center">
+                                <img src="<?php echo $serie->image ?>" alt="">
+                                <h2>TITOLO: <?php echo $serie->title ?></h2>
+                                <div>LINGUA: <?php echo $serie->language ?></div>
+                                <div>VOTAZIONE: <?php echo $serie->vote ?></div>
+                                <div>STAGIONI: <?php echo $serie->seasons ?></div>
                             </div>
                         <?php
                         }
                         ?>
 
                     </div>
-                </div>
+                
             </div>
         </main>
     </div>
